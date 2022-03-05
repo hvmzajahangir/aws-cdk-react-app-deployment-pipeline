1a12e8951254d20ae68f4221dbbef98227141f3f
Hamza <hamza@Hamzas-MacBook-Pro.local>
2022-03-05 16:36:44 +0000

    add CodePipeline to detect codebase changes, create a production build and deploy it to S3 / CloudFront

--- cdk/lib/cdk-stack.d.ts
+++ cdk/lib/cdk-stack.d.ts
@@ -1,5 +1,11 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+export interface PipelineProps extends StackProps {
+    github: {
+        owner: string;
+        repository: string;
+    };
+}
 export declare class CdkStack extends Stack {
-    constructor(scope: Construct, id: string, props?: StackProps);
+    constructor(scope: Construct, id: string, props: PipelineProps);
 }
--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -1,29 +1,27 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { Stack, StackProps, SecretValue, CfnOutput } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
-import * as iam from "aws-cdk-lib/aws-iam";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as CodeBuild from "aws-cdk-lib/aws-codebuild";
+import * as CodePipeline from "aws-cdk-lib/aws-codepipeline";
+import * as CodePipelineAction from "aws-cdk-lib/aws-codepipeline-actions";
+import * as SecretsManager from "aws-cdk-lib/aws-secretsmanager";
+
+export interface PipelineProps extends StackProps {
+  github: {
+    owner: string;
+    repository: string;
+  };
+}
 export class CdkStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: PipelineProps) {
     super(scope, id, props);
 
     // S3 bucket for static hosting
     const bucket = new s3.Bucket(this, "ReactBucket", {
-      bucketName: "cdk-deploy-react-app-demo-2022",
-      // blockPublicAccess: new s3.BlockPublicAccess({ restrictPublicBuckets: false }),
       websiteIndexDocument: "index.html",
-      // Not speciying error document can affect React Router usage
-      websiteErrorDocument: "index.html",
+      websiteErrorDocument: "index.html", // recommended to avoid bugs with React Routing etc.
     });
-
-    // // Allow public access to bucket objects
-    // const bucketIamPolicy = new iam.PolicyStatement({
-    //   actions: ['s3:GetObject'],
-    //   resources: [
-    //     `${bucket.bucketArn}/*`
-    //   ],
-    //   principals: [new iam.AnyPrincipal()],
-    // })
 
     // Restrict bucket access to CloudFront only using OAI
     const cloudFrontOriginAccessIdentity = new cloudfront.OriginAccessIdentity(
@@ -48,5 +46,75 @@ export class CdkStack extends Stack {
         ],
       }
     );
+
+    // Artifacts produced by CodeBuild
+    const outputSources = new CodePipeline.Artifact();
+    const outputApp = new CodePipeline.Artifact();
+
+    // Pipeline
+    const pipeline = new CodePipeline.Pipeline(this, "Pipeline", {
+      pipelineName: "ReactApp",
+      restartExecutionOnUpdate: true,
+    });
+
+    const githubPersonalAccessToken = SecretsManager.Secret.fromSecretNameV2(
+      this,
+      "GithubPersonalAccessToken",
+      "GithubPersonalAccessToken"
+    );
+
+    // Stage 1: clone sources from repo
+    pipeline.addStage({
+      stageName: "Source",
+      actions: [
+        new CodePipelineAction.GitHubSourceAction({
+          actionName: "Checkout",
+          repo: props.github.repository,
+          owner: props.github.owner,
+          oauthToken: githubPersonalAccessToken.secretValue,
+          branch: "main",
+          output: outputSources,
+          trigger: CodePipelineAction.GitHubTrigger.WEBHOOK,
+        }),
+      ],
+    });
+
+    // Stage 2: Build React app and CDK resources
+    pipeline.addStage({
+      stageName: "Build",
+      actions: [
+        new CodePipelineAction.CodeBuildAction({
+          actionName: "ReactApp",
+          project: new CodeBuild.PipelineProject(this, "BuildReactApp", {
+            projectName: "ReactApp",
+            buildSpec:
+              CodeBuild.BuildSpec.fromSourceFilename("./buildspec.yml"),
+            environment: {
+              buildImage: CodeBuild.LinuxBuildImage.STANDARD_5_0,
+            },
+          }),
+          input: outputSources,
+          outputs: [outputApp],
+        }),
+      ],
+    });
+
+    // Stage 3: deploy to S3
+    pipeline.addStage({
+      stageName: "Deploy",
+      actions: [
+        new CodePipelineAction.S3DeployAction({
+          actionName: "ReactApp",
+          input: outputApp,
+          bucket: bucket,
+        }),
+      ],
+    });
+
+    new CfnOutput(this, "AppURL", {
+      value: distribution.distributionDomainName,
+      description: "Web app URL",
+      exportName: "AppURL",
+    });
   }
 }
